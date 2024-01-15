6f222768fb3fb268655d08ba9e5c02bd78c6b114
Aryan Khokhar <aryankhokhar2000@gmail.com>
2024-01-15 21:57:57 +0530

    Adding Practice1_3.java

---
+++ Practice1_3.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Practice1_3 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Name: ");
+        String name = sc.nextLine();
+        System.out.println("Hello "+name+", have a good day!");
+    }
+}
