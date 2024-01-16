868c823ded54827d10fc9850b0dea8f0437a882e
Aryan Khokhar <aryankhokhar2000@gmail.com>
2024-01-16 21:35:27 +0530

    Adding chapter2_precidence_and_association.java

---
+++ Chapter2_Precedence_and_Associativity.java
@@ -0,0 +1,13 @@
+class Chapter2_Precedence_and_Associativity {
+    public static void main(String[] args) {
+        // Precedence and Associativity
+        
+        // It define which operator will be executed.
+        
+        int a = 20/5-8*3;
+        System.out.println(a);
+
+        int b = 20*5-8/3;
+        System.out.println(b);
+    }
+}
