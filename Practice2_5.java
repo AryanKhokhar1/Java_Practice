5c2f0695bf7128a2c4929f1d1378f9e54bfe4f9a
Aryan Khokhar <aryankhokhar2000@gmail.com>
2024-01-17 20:47:23 +0530

    Adding Practice 2_5.java

---
+++ Practice2_5.java
@@ -0,0 +1,8 @@
+public class Practice2_5 {
+    public static void main(String[] args) {
+        System.out.println("Expression Evaluation: ");
+        int x = 7;
+        int a = (int) 7.49/x + 35/x;
+        System.out.println("Expression Result: "+a);
+    }
+}
