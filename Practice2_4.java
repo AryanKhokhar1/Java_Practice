f2f2e444f7ead5439aebc6f47e6170859a00374a
Aryan Khokhar <aryankhokhar2000@gmail.com>
2024-01-17 20:47:14 +0530

    Adding Practice 2_4.java

---
+++ Practice2_4.java
@@ -0,0 +1,11 @@
+public class Practice2_4 {
+    public static void main(String[] args) {
+        int v = 8;
+        int u = 2;
+        int a = 1;
+        int s = 3;
+        System.out.println("Expression evaluation: ");
+        float answer = (v*v - u*u)/(2.0f*a*s);
+        System.out.println(answer);
+    }
+}
