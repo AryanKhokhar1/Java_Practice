05d56297d94f185453df7f4db5244b44b460b5ea
Aryan Khokhar <aryankhokhar2000@gmail.com>
2024-01-17 13:42:44 +0530

    Adding Chapter2_resultant_datatype.java

--- Chapter2_resultant_datatype.java
+++ Chapter2_resultant_datatype.java
@@ -1,5 +1,8 @@
 class Chapter2_resultant_datatype {
     public static void main(String[] args) {
+
+        // Resultant datatype after operation performed on different different datatypes
+
         int i = 4;
         float f = 3.4f;
         byte b = 1;
@@ -12,5 +15,19 @@ class Chapter2_resultant_datatype {
         System.out.println(d+f);  // It'will return a double number
         System.out.println(l+b);  // It'will return a long number
         // System.out.println((i+i).getClass().getName());
+
+        
+        // Increment and Decrement Operators
+        int x = 4;
+        int y = 8;
+        // int z =18;
+        char character = 'a';
+        System.out.println(x++); // print first then increment
+        System.out.println(x);
+        System.out.println(++y); // first increment then print
+        System.out.println(y);
+        System.out.println(y++*9);
+        System.out.println(++character);
+
     }
 }
