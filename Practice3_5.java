ba833a90b0056646dc4a3a57ac40514bf9ffeb2c
Aryan Khokhar <aryankhokhar2000@gmail.com>
2024-01-24 23:33:47 +0530

    Adding Practice3_5.java

---
+++ Practice3_5.java
@@ -0,0 +1,8 @@
+// Using of Escape Sequence
+
+public class Practice3_5 {
+    public static void main(String[] args) {
+        String letter = "Dear Aryan,\nThis Java Course is nice.\nThanks";
+        System.out.println(letter);
+    }
+}
