04f2d2309dd48c4c8c6124eafd699d6f0347bc0f
Aryan Khokhar <aryankhokhar2000@gmail.com>
2024-01-24 23:33:20 +0530

    Adding Practice3_2.java

---
+++ Practice3_2.java
@@ -0,0 +1,10 @@
+// Replace " " to "_"
+
+
+public class Practice3_2 {
+    public static void main(String args[]){
+        String name =  "      Aryan Khokhar     ";
+        name = name.replace(" ","_");
+        System.out.println(name);
+    }
+}
