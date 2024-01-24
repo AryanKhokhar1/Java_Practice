53546725d520145071b7fed14dce6d1bdfbda6af
Aryan Khokhar <aryankhokhar2000@gmail.com>
2024-01-24 19:41:37 +0530

    some changes

--- Chapter3_String_method.java
+++ Chapter3_String_method.java
@@ -3,7 +3,7 @@ public class Chapter3_String_method {
         String name = "Aryan Khokhar";
         String nonTrimmedString = "    Aryan Khokhar   ";
 
-        /*
+        
         System.out.println(name.length());
         System.out.println(name.toLowerCase());
         System.out.println(name.toUpperCase());
@@ -16,7 +16,7 @@ public class Chapter3_String_method {
         System.out.println(name.replace('r','R')); // Replace in whole string 
         System.out.println(name.replace("ry","RY"));  
         // Remember in Java single comma used for character and double comma used for String 
-        */
+        
         System.out.println(name.startsWith("Ar"));
         System.out.println(name.endsWith("har"));
 
@@ -25,10 +25,14 @@ public class Chapter3_String_method {
         System.out.println(name.indexOf("Kh",2)); // indexof function works on both char and str
 
         System.out.println(name.lastIndexOf("r")); // It start finding from backward
-
+        System.out.println(name.lastIndexOf("r",5));
         System.out.println(name.equals("Aryan Khokhar")); // It check given string is same as defined string
 
         System.out.println(name.equalsIgnoreCase("aryan khokhar")); // Check here isn't case sensitive
 
+
+        // Escape Sequence (Use of /)
+
+        System.out.println("Print \\ \n for next line \t \'");
     }
 }
