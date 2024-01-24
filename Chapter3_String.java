63eb3200477ef8ad1e9813bbe216ed6d1bbf2a0f
Aryan Khokhar <aryankhokhar2000@gmail.com>
2024-01-24 19:05:23 +0530

    Adding Chapter3_String.java

--- Chapter3_String.java
+++ Chapter3_String.java
@@ -8,5 +8,11 @@ public class Chapter3_String {
 
         String religion = "Sanatan";
         System.out.println("\nReligion of "+name+" is "+religion);
+
+        System.out.format("\nReligion of %s is %s",name,religion);  // format printing method is same as prinf method
+
+
+        float number = 2.48373874f;
+        System.out.printf("Float number = %8.2f",number);  // here 8 define number of space taken by number and 2 define number of digit after point
     }
 }
