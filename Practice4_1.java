f7ac2e1810c19c2e8fe53e83f74a90a52bdfb61f
Aryan Khokhar <aryankhokhar2000@gmail.com>
2024-01-25 20:34:10 +0530

    Adding Practice4_1.java

---
+++ Practice4_1.java
@@ -0,0 +1,9 @@
+public class Practice4_1 {
+    public static void main(String[] args) {
+        int a = 10;
+        if(a == 11)
+            System.out.println("I am 11");
+        else
+            System.out.println("I am not 11");
+    }
+}
