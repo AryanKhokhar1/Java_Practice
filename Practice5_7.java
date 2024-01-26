ac6849c1b91cbda53a36c86ea319dd72352be854
Aryan Khokhar <aryankhokhar2000@gmail.com>
2024-01-27 03:39:26 +0530

    Adding Practice5_7.java

---
+++ Practice5_7.java
@@ -0,0 +1,14 @@
+public class Practice5_7 {
+    public static void main(String[] args) {
+        int n = 4;
+        while(n >0){
+            int a = n;
+            while(a>0){
+                System.out.print("*");
+                a--;
+            }
+            System.out.println();
+            n--;
+        }
+    }
+}
