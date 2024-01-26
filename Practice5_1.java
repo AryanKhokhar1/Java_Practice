3b09b49a54932b071322da5c2f8afa325707d55b
Aryan Khokhar <aryankhokhar2000@gmail.com>
2024-01-27 03:20:56 +0530

    Adding Practice5_1.java

---
+++ Practice5_1.java
@@ -0,0 +1,11 @@
+public class Practice5_1 {
+    public static void main(String[] args) {
+        char special_symbol = '*';
+        for(int i = 4; i>0; i--){
+            for(int j = i; j>0; j--){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
