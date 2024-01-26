bea11d682a3dd506c3040ecd3096dd79f5310e13
Aryan Khokhar <aryankhokhar2000@gmail.com>
2024-01-27 01:01:36 +0530

    Change in Practice4_6.java

--- Practice4_6.java
+++ Practice4_6.java
@@ -5,11 +5,11 @@ public class Practice4_6 {
         String url = "www.google.com";
         System.out.println("Type of Website");
         System.out.println("URL: "+url);
-        if (url.contains(".com")) {
+        if (url.endsWith(".com")) {
             System.out.println("It's a global site");
-        }else if(url.contains(".in")){
+        }else if(url.endsWith(".in")){
             System.out.println("This website is only for indians");
-        }else if(url.contains(".org")){
+        }else if(url.endsWith(".org")){
             System.out.println("This site run by an Organization");
         }
     }
