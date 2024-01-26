88feb84d5367fdc169fff1a2df41e644158998d8
Aryan Khokhar <aryankhokhar2000@gmail.com>
2024-01-27 01:46:08 +0530

    Adding Chapter5_whileloop.java

---
+++ Chapter5_whileloop.java
@@ -0,0 +1,9 @@
+public class Chapter5_whileloop {
+    public static void main(String[] args) {
+        int num = 100;
+        while (num<=200) {
+            System.out.println(num);
+            num++;
+        }
+    }
+}
