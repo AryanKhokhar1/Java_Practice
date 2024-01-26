8bdeb85d58b735fa37c5d438f01b1faa49459e31
Aryan Khokhar <aryankhokhar2000@gmail.com>
2024-01-27 03:49:24 +0530

    Practice5_2.java added

--- Practice5_2.java
+++ Practice5_2.java
@@ -6,10 +6,13 @@ public class Practice5_2 {
         System.out.println("Suming of n even numbers");
         System.out.print("N: ");
         int num = sc.nextInt();
+        int sum = 0;
         int n  = 0;
         while(n<num){
             System.out.println(2*n);
+            sum += 2*n;
             n++;
         }
+        System.out.println("Addition of "+num+" even number = "+sum);
     }
 }
