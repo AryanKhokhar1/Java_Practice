d8fa1e0c6a0de6dd4dde0f7c31b6ec1b82829e56
Aryan Khokhar <aryankhokhar2000@gmail.com>
2024-01-27 03:45:15 +0530

    Adding  Practice5_9.java

---
+++ Practice5_9.java
@@ -0,0 +1,10 @@
+public class Practice5_9 {
+    public static void main(String[] args) {
+        int num = 8;
+        int add = 0;
+        for(int i = 1; i<=10; i++){
+            add += num*i;
+        }
+        System.out.println("Total Addition: "+add);
+    }
+}
