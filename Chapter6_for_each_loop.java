07e0671a0855ae6bb8fce637b7c9883a201356c4
Aryan Khokhar <aryankhokhar2000@gmail.com>
2024-01-27 21:08:53 +0530

    Chapter6_for_each_loop.java added

---
+++ Chapter6_for_each_loop.java
@@ -0,0 +1,12 @@
+// For each Loop
+
+public class Chapter6_for_each_loop {
+    public static void main(String[] args) {
+        String[] Name = {"Aryan Khokhar", "Lavish Khokhar", "Siddhant Diweddi", "Rich Shrivastav"};
+
+        // It's For each loop
+        for(String element : Name){
+            System.out.println(element);
+        }
+    }
+}
