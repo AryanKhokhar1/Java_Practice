3ebc78afb4aa0f1e166e0f4eb1b0fcaca516928f
Aryan Khokhar <aryankhokhar2000@gmail.com>
2024-01-31 16:19:40 +0530

    Practice7_8.java added

---
+++ Practice7_8.java
@@ -0,0 +1,16 @@
+public class Practice7_8 {
+    static String printpattern(int num , int x){
+        if(x == 0){
+            return "";
+        }
+        for(int i = 0; i<num; i++){
+            System.out.print("* ");
+        }
+        System.out.println();
+        return printpattern(num+1,x-1);
+    }
+    public static void main(String[] args) {
+        int num = 5;
+        printpattern(1,num);
+    }
+}
