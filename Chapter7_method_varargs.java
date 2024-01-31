1de654ffd34be41f163029e85ba196e5ab01bad1
Aryan Khokhar <aryankhokhar2000@gmail.com>
2024-01-31 15:36:10 +0530

    Chapter 7 method varargs added

--- Chapter7_method_varargs.java
+++ Chapter7_method_varargs.java
@@ -13,7 +13,17 @@ public class Chapter7_method_varargs {
         }
         return result;
     }
+
+    // Here we define one argument is must
+    static int sum_oneargismust(int x, int ...arr){
+        int result = x;
+        for(int element: arr){
+            result += element;
+        }
+        return result;
+    }
     public static void main(String[] args) {
+        System.out.println("The sum of 2 and 3: "+sum_oneargismust(2,3));
         System.out.println("The sum of 2 and 3: "+sum(2,3));
         System.out.println("The sum of 2,4 and 3: "+sum(2,4,3));
         System.out.println("The sum of 2,4,5 and 3: "+sum(2,4,5,3));
