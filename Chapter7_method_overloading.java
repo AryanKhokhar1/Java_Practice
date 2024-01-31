734e0892042a9ea2a09a37768fa3247c3f02efbe
Aryan Khokhar <aryankhokhar2000@gmail.com>
2024-01-31 14:58:25 +0530

    Chapter7 varargs added

--- Chapter7_method_overloading.java
+++ Chapter7_method_overloading.java
@@ -1,24 +1,24 @@
-public class Chapter7_array_pass_in_method {
+public class Chapter7_method_overloading {
 
 
     // Here x will copy value of a  // so change in x doesn't affect a
-    static int Changeint(int x){
+    static int Change(int x){ // int x is paramenter 
         x = 45;
         return x;
     }
 
     // Here num pass reference to arr  // so change in arr reflect to num
-    static void Changearray(int[] arr){
+    static void Change(int[] arr){
         arr[0] = 34;
     }
     public static void main(String[] args) {
         int a = 89;
         System.out.println("Value of a before runnning change method = "+a);
-        Changeint(a);
+        Change(a); // In Java it select method automatically based on datatype passed
         System.out.println("Value of a after runnning change method = "+a);
-        int[] num = {23, 78, 98};
+        int[] num = {23, 78, 98}; // Here num store reference / address 
         System.out.println("Value of array 1st element before runnning change method = "+num[0]);
-        Changearray(num);
+        Change(num); // num is an argument
         System.out.println("Value of array 1st element after runnning change method = "+num[0]);
 
     }
---
+++ Chapter7_method_varargs.java
@@ -0,0 +1,23 @@
+/**
+ * Chapter7_method_varargs 
+ */
+
+
+public class Chapter7_method_varargs {
+    
+    // Here are variable argument not defined number of argument
+    static int sum(int ...arr){ // It store argument into an array named as arr
+        int result = 0;
+        for(int element: arr){
+            result += element;
+        }
+        return result;
+    }
+    public static void main(String[] args) {
+        System.out.println("The sum of 2 and 3: "+sum(2,3));
+        System.out.println("The sum of 2,4 and 3: "+sum(2,4,3));
+        System.out.println("The sum of 2,4,5 and 3: "+sum(2,4,5,3));
+        System.out.println("The sum of 2,4,5,6 and 3: "+sum(2,4,5,6,3));
+
+    }
+}
