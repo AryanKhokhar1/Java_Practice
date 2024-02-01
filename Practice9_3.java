af8721a0817bb047a3b3117ca48da32a2f26652c
Aryan Khokhar <aryankhokhar2000@gmail.com>
2024-02-01 19:05:52 +0530

    Practice9_3.java added

---
+++ Practice9_3.java
@@ -0,0 +1,39 @@
+class Cylinder{
+    private float height;
+    private float radius;
+    Cylinder(){
+
+    }
+    Cylinder(float height, float radius){
+        this.height = height;
+        this.radius = radius;
+    }
+    float getHeight() {
+        return height;
+    }
+    float getRadius() {
+        return radius;
+    }
+    void setHeight(float height) {
+        this.height = height;
+    }
+    void setRadius(float radius) {
+        this.radius = radius;
+    }
+    float surfaceArea(){
+        return ((2*22*this.radius*this.radius)/7.0f) + ((2*22*this.radius*this.height)/7.0f);
+    }
+    float volume(){
+        return (22*this.radius*this.radius*this.height)/7.0f;
+    }
+}
+
+public class Practice9_3 {
+  public static void main(String[] args) {
+    Cylinder cyl = new Cylinder(10.56f,3.86f);
+    System.out.println("Height of the Cylinder: "+cyl.getHeight());
+    System.out.println("Radius of the Cylinder: "+cyl.getRadius());
+    System.out.format("\n Total Surface Area of the Cylinder: %.3f",cyl.surfaceArea());
+    System.out.printf("\n Volume of the Cylinder: %.3f",cyl.volume());
+  }
+}
