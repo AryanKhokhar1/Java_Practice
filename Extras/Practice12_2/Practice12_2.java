fdbfd04404c634edba8dfc394a765b9bb19d21ad
Aryan Khokhar <aryankhokhar2000@gmail.com>
2024-02-19 00:25:08 +0530

    Practice12_3 added

---
+++ Extras/Practice12_2/Practice12_2.java
@@ -0,0 +1,11 @@
+
+package Practice12_2;
+import java.util.Scanner;
+public class Practice12_2 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Name: ");
+        String name = scan.nextLine();
+        System.out.println("Hii, "+name+" Welcome to My Program!");
+    }
+}
---
+++ Practice12_3/folder1/folder2/Practice12_3.java
@@ -0,0 +1,7 @@
+package Practice12_3.folder1.folder2;
+
+public class Practice12_3 {
+    public static void main(String[] args) {
+        
+    }
+}
