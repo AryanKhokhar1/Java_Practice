652fdcab135b4903d0abdf48f64c243b99328747
Aryan Khokhar <aryankhokhar2000@gmail.com>
2024-02-20 21:35:57 +0530

    Chapter13_Thread_Method added

---
+++ Chapter13_Thread_Method.java
@@ -0,0 +1,33 @@
+
+class MyThread1 extends Thread {
+
+    public void run(){
+        while(true){
+            System.out.println("Thread 1 is running...");
+        }
+    }
+}
+class MyThread2 extends Thread {
+
+    public void run(){
+        while(true){
+            System.out.println("Thread 2 is running...");
+        }
+    }
+}
+public class Chapter13_Thread_Method {
+    public static void main(String[] args) {
+        MyThread1 mt1 = new MyThread1();
+        MyThread2 mt2 = new MyThread2();
+        mt1.start();
+
+        // after appling join method on MyThread1 object . MyThread1 object will run continously without intercepting by MyThread2 object 
+        try {
+            mt1.join();
+        } catch (Exception e) {
+            // TODO: handle exception
+            System.out.println(e);
+        }
+        mt2.start();
+    }
+}
