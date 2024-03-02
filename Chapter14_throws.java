c1343a95f5ca2c4cc7211418f6ae0d7aae6db229
Aryan Khokhar <aryankhokhar2000@gmail.com>
2024-03-02 12:36:56 +0530

    Chapter14_throws.java added

--- Chapter14_throws.java
+++ Chapter14_throws.java
@@ -1,5 +1,36 @@
+class NegativeDimentionException extends Exception{
+     @Override
+     public String toString() {
+         return "Dimention can't be negative";
+     }
+     @Override
+     public String getMessage() {
+         return "Dimension can't be negative";
+     }
+}
+
+class Rectangle{
+
+    double Area(double length, double width) throws NegativeDimentionException{
+        if(length<0 || width <0){
+            NegativeDimentionException child = new NegativeDimentionException();
+            throw child;
+        }
+        double area = length * width;
+        return area;
+    }
+}
 public class Chapter14_throws {
     public static void main(String[] args) {
-        
+        Rectangle rec = new Rectangle();
+        try{
+            double value = rec.Area(5, -7);
+            System.out.printf("Area of Rectangle: %.2f ",value);
+        }
+        catch(Exception e){
+            System.out.println(e);
+            System.out.println(e.toString());
+            System.out.println(e.getMessage());
+        }
     }
 }
